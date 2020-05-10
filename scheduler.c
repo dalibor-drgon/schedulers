fb84896ba00636ca62112d575afeefa84972590c
Dalibor Drgon <dalibor.drgon@gmail.com>
2020-05-10 14:46:36 +0200

    version 0.20: implement lazy EDF and lazy RMS scheduling

---
+++ scheduler.c
@@ -0,0 +1,401 @@
+
+#include "scheduler.h"
+#include <stddef.h>
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/scb.h>
+
+sched scheduler;
+
+/**************************** List functions **********************************/
+
+static void list_unlink(sched_list *list, sched_task *task) {
+    if(list->first == task && list->last == task) {
+        list->first = NULL;
+        list->last = NULL;
+    } else {
+        sched_task *prev = task->sched_prev;
+        sched_task *next = task->sched_next;
+        if(prev != NULL) prev->sched_next = next;
+        if(next != NULL) next->sched_prev = prev;
+        if(list->first == task) list->first = task->sched_next;
+        if(list->last  == task) list->last  = task->sched_prev;
+        task->sched_prev = task->sched_next = NULL;
+    }
+} 
+
+static void list_append(sched_list *list, sched_task *task) {
+    if(list->first == NULL /* && list->last == NULL */) {
+        list->first = list->last = task;
+    } else {
+        sched_task *prev = list->last;
+        prev->sched_next = task;
+        task->sched_prev = prev;
+        list->last = task;
+    }
+}
+
+/**
+ * @brief Exchanges position of given two tasks in a list.
+ * 
+ * @param list Original list
+ * @param cur nth task in given list
+ * @param prev (n-1)th task in given list
+ */
+static void list_exchange(sched_list *list, sched_task *cur, sched_task *prev) {
+    if(list->last == cur) {
+        list->last = prev;
+        prev->sched_next = NULL;
+    } else {
+        prev->sched_next = cur->sched_next;
+    }
+    if(list->first == prev) {
+        list->first = cur;
+        cur->sched_prev = NULL;
+    } else {
+        cur->sched_prev = prev->sched_prev;
+    }
+    cur->sched_next = prev;
+    prev->sched_prev = cur;
+}
+
+static void list_bubbleup(sched_list *list, sched_task *task, 
+        bool (*is_lower)(sched_task *one, sched_task *two))
+{
+    sched_task *prev = task->sched_prev;
+    while(prev != NULL) {
+        if(is_lower(task, prev)) {
+            list_exchange(list, task, prev);
+        } else break;
+
+        prev = task->sched_prev;
+    }
+}
+
+static void list_bubbledown(sched_list *list, sched_task *task, 
+        bool (*is_lower)(sched_task *one, sched_task *two)) 
+{
+    /// This sequence is same as bubble down, but the performance may be better
+    /// or worse, depending on the variance of the intervals.
+    list_unlink(list, task);
+    list_append(list, task);
+    list_bubbleup(list, task, is_lower);
+}
+
+
+/**************************** Queue functions *********************************/
+
+static sched_task *queue_dequeue(sched_queue *queue) {
+    int irq = sched_irq_disable();
+    if(queue->first == NULL) {
+        if(!irq) sched_irq_enable();
+        return NULL;
+    }
+    sched_task *task = queue->first;
+    if(queue->first == task && queue->last == task) {
+        queue->first = queue->last = NULL;
+    } else {
+        queue->first = task->fired_next;
+    }
+    task->fired_next = NULL;
+
+    if(!irq) sched_irq_enable();
+    return task;
+}
+
+static void queue_enqueue(sched_queue *queue, sched_task *task) {
+    int irq = sched_irq_disable();
+    if(queue->first == NULL /* && queue->last == NULL */) {
+        queue->first = queue->last = task;
+    } else {
+        sched_task *prev = queue->last;
+        prev->fired_next = task;
+        queue->last = task;
+    }
+    if(!irq) sched_irq_enable();
+}
+
+
+/**************************** List comparators ********************************/
+
+static bool list_rt_islower(sched_task *one, sched_task *two) {
+    // if(one->state == SCHEDSTATE_READY && two->state != SCHEDSTATE_READY) {
+    //     return true;
+    // }
+    // if(one->state != SCHEDSTATE_READY && two->state == SCHEDSTATE_READY) {
+    //     return false;
+    // }
+#ifdef SCHEDULER_USE_RMS
+    uint32_t interval_one = one->data.realtime.interval;
+    uint32_t interval_two = two->data.realtime.interval;
+    return (interval_one < interval_two);
+#else 
+    uint32_t deadline_one = one->data.realtime.next_execution + one->data.realtime.interval;
+    uint32_t deadline_two = two->data.realtime.next_execution + two->data.realtime.interval;
+    uint32_t ticks = sched_ticks();
+    int32_t rem_one = deadline_one - ticks;
+    int32_t rem_two = deadline_two - ticks;
+    return (rem_one < rem_two);
+#endif
+}
+
+static bool list_rtw_islower(sched_task *one, sched_task *two) {
+    uint32_t ticks = sched_ticks();
+    int32_t rem_one = one->data.realtime.next_execution - ticks;
+    int32_t rem_two = two->data.realtime.next_execution - ticks;
+    return rem_one < rem_two;
+}
+
+
+#if 0
+static bool list_nm_islower(sched_task *one, sched_task *two) {
+    if(one->state == SCHEDSTATE_READY && two->state != SCHEDSTATE_READY) {
+        return true;
+    }
+    if(one->state != SCHEDSTATE_READY && two->state == SCHEDSTATE_READY) {
+        return false;
+    }
+    return one->priority > two->priority;
+}
+#endif
+
+/**************************** Scheduler functions *****************************/
+
+void sched_init() {
+    scheduler.realtime_tasks.first = scheduler.realtime_tasks.last = NULL;
+    scheduler.fired_tasks.first = scheduler.fired_tasks.last = NULL;
+    scheduler.cur_task = &scheduler.main_task;
+}
+
+void sched_start() {
+    sched_syscall(NULL, NULL);
+}
+
+int __attribute__((noinline)) sched_syscall(
+    sched_syscall_function syscall_function,
+    void *data)
+{
+	/* Trigger PendSV, causing pend_sv_handler to be called immediately */
+	SCB_ICSR |= SCB_ICSR_PENDSVSET;
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+    (void) syscall_function, (void) data;
+    // Return value will be set by given syscall, do not worry about the
+    // warning. Tested with gcc, may require modification with other compilers.
+}
+
+static bool sched_nexttask() {
+    // Add fired tasks from queue into realtime_tasks_waiting list
+    sched_task *task;
+    do {
+        // Dequeue task
+        task = queue_dequeue(&scheduler.fired_tasks);
+        if(task == NULL) break;
+        list_append(&scheduler.realtime_tasks_waiting, task);
+        list_bubbleup(&scheduler.realtime_tasks_waiting, task, list_rtw_islower);
+    } while(true);
+
+    // Move no-longer-waiting tasks into task pending list `realtime_tasks`
+    uint32_t ticks = sched_ticks();
+    do {
+        // If there are no waiting tasks, break
+        task = scheduler.realtime_tasks_waiting.first;
+        if(task == NULL) break;
+
+        // If current task is still waiting, break
+        int32_t rem_ticks = task->data.realtime.next_execution - ticks;
+        if(rem_ticks > 0) break; 
+
+        list_unlink(&scheduler.realtime_tasks_waiting, task);
+        list_append(&scheduler.realtime_tasks, task);
+        list_bubbleup(&scheduler.realtime_tasks, task, list_rt_islower);
+    } while(true);
+
+    if(scheduler.realtime_tasks.first == NULL) return false;
+    
+    // Pick process
+    scheduler.cur_task = scheduler.realtime_tasks.first;
+    int32_t rem = scheduler.cur_task->data.realtime.next_execution - sched_ticks();
+    return rem <= 0;
+}
+
+static void sched_movetask() {
+    list_unlink(&scheduler.realtime_tasks, scheduler.cur_task);
+}
+
+static void sched_restoretask() {
+    // if(scheduler.cur_task->state == SCHEDSTATE_READY) {
+        list_append(&scheduler.realtime_tasks, scheduler.cur_task);
+        list_bubbleup(&scheduler.realtime_tasks, scheduler.cur_task, list_rt_islower);
+    // }
+}
+
+/**************************** Task privileged functions ***********************/
+// Can be run only at initialization phase or from syscall
+
+static void return_function();
+
+static void sched_taskp_reinit(sched_task *task) {
+    task->sp = task->sp_end - sizeof(sched_stack);
+
+    sched_stack *stack = (sched_stack *) task->sp;
+    stack->psr = 0x21000000;
+    stack->pc = task->entry_function;
+    stack->lr = return_function;
+    stack->r0 = (uint32_t) task->function_data;
+}
+
+void sched_taskp_tick(sched_task *task) {
+    task->data.realtime.next_execution += task->data.realtime.interval;
+    // list_bubbledown(&scheduler.realtime_tasks, scheduler.realtime_tasks.first,
+    //         list_rt_islower);
+}
+
+/**************************** Task built-in syscalls **************************/
+
+static bool sched_task_tick_reinit_syscall(void *data, sched_task *task) {
+    (void) data;
+    task->state = SCHEDSTATE_READY;
+    sched_taskp_tick(task);
+    sched_taskp_reinit(task);
+    sched_task_fire(task, 0);
+    return false;
+}
+
+static bool sched_task_tick_syscall(void *data, sched_task *task) {
+    (void) data;
+    task->state = SCHEDSTATE_READY;
+    sched_taskp_tick(task);
+    sched_task_fire(task, 0);
+    return false;
+}
+
+/**************************** Task functions **********************************/
+
+void sched_task_tick() {
+    sched_syscall(sched_task_tick_syscall, NULL);
+}
+
+void sched_task_init(sched_task *task,
+        uint8_t *sp, unsigned sp_length,
+        sched_entry_function function, void *data
+) {
+    task->sp_end = sp + sp_length;
+    task->entry_function = function;
+    task->function_data = data;
+    sched_taskp_reinit(task);
+
+    task->sched_prev = task->sched_next = task->fired_next = NULL;
+}
+
+void sched_task_add(sched_task *task,
+        uint32_t next_execution, uint32_t interval) {
+    task->state = SCHEDSTATE_READY;
+    task->data.realtime.next_execution = next_execution;
+    task->data.realtime.interval = interval;
+    queue_enqueue(&scheduler.fired_tasks, task);
+    // list_append(&scheduler.realtime_tasks, task);
+    // list_bubbleup(&scheduler.realtime_tasks, task, list_rt_islower);
+}
+
+void sched_task_fire(sched_task *task, int return_value) {
+    // Set state to READY
+    task->state = SCHEDSTATE_READY;
+    
+    // Set return value
+    sched_stack *stack = (sched_stack *) task->sp;
+    stack->r0 = return_value;
+
+    // Finally enqueue it into fired tasks queue to be processed from PendSV
+    // handler 
+    queue_enqueue(&scheduler.fired_tasks, task);
+}
+
+
+/**************************** PendSV handler **********************************/
+
+static void return_function() {
+    sched_syscall(sched_task_tick_reinit_syscall, NULL);
+}
+
+void __attribute__((__naked__)) PendSV_Handler() {
+    const uint32_t RETURN_ON_PSP = 0xfffffffd;
+
+	/* 0. NVIC has already pushed some registers on the program/main stack.
+	 * We are free to modify R0..R3 and R12 without saving them again, and
+	 * additionally the compiler may choose to use R4..R11 in this function.
+	 * If it does so, the naked attribute will prevent it from saving those
+	 * registers on the stack, so we'll just have to hope that it doesn't do
+	 * anything with them before our stm or after our ldm instructions.
+	 * Luckily, we don't ever intend to return to the original caller on the
+	 * main stack, so this question is moot. */
+
+	/* Read the link register */
+	uint32_t lr;
+	__asm__("MOV %0, lr" : "=r" (lr));
+
+	if (lr & 0x4) {
+		/* This PendSV call was made from a task using the PSP */
+
+		/* 1. Push all other registers (R4..R11) on the program stack */
+		void *psp;
+		__asm__(
+			/* Load PSP to a temporary register */
+			"MRS %0, psp\n"
+			/* Push context relative to the address in the temporary
+			 * register, update register with resulting address */
+			"STMDB %0!, {r4-r11}\n"
+			/* Put back the new stack pointer in PSP (pointless) */
+			"MSR psp, %0\n"
+			: "=r" (psp));
+
+		/* 2. Store that PSP in the current TCB */
+	    scheduler.cur_task->sp = psp;
+	} else {
+		/* This PendSV call was made from a task using the MSP. This
+		 * code is not equipped to return to the main task, but we store
+		 * the proper registers here anyway for good form. */
+
+		/* 1. Push all other registers (R4..R11) on the main stack */
+        void *sp;
+		__asm__(
+			/* Push context on main stack */
+			"STMDB SP!, {r4-r11}\n\t"
+            "MOV %0, SP" 
+            : "=r" (sp));
+		scheduler.cur_task->sp = sp;
+	}
+
+	/* 3. Call context switch function, changes current TCB */
+    register sched_syscall_function syscall_func asm ("r0");
+    register void * data asm ("r1");
+    if(syscall_func != NULL) {
+        scheduler.cur_task->state = SCHEDSTATE_BLOCKING;
+        sched_movetask();
+        if(syscall_func(data, scheduler.cur_task) == false) {
+            while(sched_nexttask() == false);
+        } else {
+            sched_restoretask();
+        }
+        //     sched_stack *stack = (sched_stack *) scheduler.cur_task->sp;
+        //     stack->r0 = ret;
+    } else {
+        while(sched_nexttask() == false);
+    }
+
+
+	/* 4. Load PSP from TCB */
+	/* 5. Pop R4..R11 from the program stack */
+	void *psp = scheduler.cur_task->sp;
+	__asm__(
+		/* Pop context relative temporary register, update register */
+		"LDMIA %0!, {r4-r11}\n"
+		/* Put back the stack pointer in PSP */
+		"MSR psp, %0\n"
+		: : "r" (psp));
+
+	/* 6. Return. NVIC will pop registers and find the PC to use there. */
+	__asm__("bx %0" : : "r"(RETURN_ON_PSP));
+}
---
+++ scheduler.h
@@ -0,0 +1,183 @@
+
+
+#ifndef __SCHEDULER_H
+#define __SCHEDULER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+// #include "round_robin.h"
+// #include "edf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Task structure
+struct sched_task;
+typedef struct sched_task sched_task;
+
+/// Scheduler structure
+struct sched;
+typedef struct sched sched;
+
+/// Stack inside PendSV and SysTick exception
+struct sched_stack;
+typedef struct sched_stack sched_stack;
+
+typedef bool (*sched_syscall_function)(void *data, sched_task *task);
+typedef void (*sched_entry_function)(void *data);
+
+struct sched_stack {
+	/// Registers pushed onto the stack by software by "STMDB x!, {r4-r11}".
+    /// Pushed after the the hardware (NVIC) pushes its registers.
+    struct {
+        uint32_t r4;
+        uint32_t r5;
+        uint32_t r6;
+        uint32_t r7;
+        uint32_t r8;
+        uint32_t r9;
+        uint32_t r10;
+        uint32_t r11;
+    };
+
+	/// Registers pushed on the stack by NVIC (by hardware) before we push other
+	/// registers.
+    struct {
+        uint32_t r0;
+        uint32_t r1;
+        uint32_t r2;
+        uint32_t r3;
+        uint32_t r12;
+        void *lr;
+        void *pc;
+        uint32_t psr;
+    };
+};
+
+extern sched scheduler;
+
+/// Contains state of task
+typedef enum sched_task_state {
+    /// The task is either running or waiting to be run - check
+    /// scheduler.cur_task to find out.
+    SCHEDSTATE_READY = 0, 
+    /// Waiting for syscall/IO/sleep to finish.
+    SCHEDSTATE_BLOCKING = 1
+} sched_task_state;
+
+typedef struct sched_task_realtime {
+    uint32_t next_execution;
+    uint32_t interval;
+} sched_task_realtime;
+
+struct sched_task {
+    sched_task *sched_prev, *sched_next;
+    sched_task *fired_next;
+
+    void *sp_end, *sp;
+    sched_task_state state;
+    uint32_t _pad0:24;
+
+    union {
+        sched_task_realtime realtime;
+    } data;
+
+    sched_entry_function entry_function;
+    void *function_data;
+};
+
+typedef struct sched_list {
+    sched_task *first, *last;
+} sched_list;
+
+typedef struct sched_queue {
+    sched_task *first, *last;
+} sched_queue;
+
+struct sched {
+    /**
+     * @brief This is updated with a pointer to the task that is currently
+     * executed.
+     */
+    sched_task *cur_task;
+
+    /**
+     * @brief Tasks using realtime (RMS or EDF) scheduler. This linked list is
+     * sorted using Bubble Sort as Min-Max meaning the task next to be executed
+     * is `*first` and the task last to be executed is `*last`.
+     */
+    sched_list realtime_tasks;
+
+    /**
+     * @brief Realtime tasks that are to be added to the `realtime_tasks` list
+     * once their time comes (once task.next_execution ).
+     */
+    sched_list realtime_tasks_waiting;
+
+    /**
+     * @brief (Realtime, Normal or Low-priority) tasks that were
+     * blocking/waiting but the operation which caused them to block/wait
+     * finished. FIFO queue.
+     */
+    sched_queue fired_tasks;
+
+    /**
+     * @brief Main thread (which initializes threads and starts the
+     * context-switching).
+     */
+    sched_task main_task;
+};
+
+/**************************** Dependencies ************************************/
+
+extern uint32_t sched_ticks();
+extern int sched_irq_disable();
+extern void sched_irq_enable();
+
+/**************************** Scheduler ***************************************/
+
+void sched_init();
+
+void sched_start();
+
+int sched_syscall(
+    sched_syscall_function syscall_function,
+    void *data);
+
+/**************************** Task functions **********************************/
+
+void sched_task_init(sched_task *task,
+        uint8_t *sp, unsigned sp_length,
+        sched_entry_function function, void *data
+);
+
+void sched_task_add(sched_task *task,
+        uint32_t next_execution, uint32_t interval);
+
+void sched_task_fire(sched_task *task, int return_value);
+
+/**
+ * @brief For use in realtime tasks. Use this to signal the end of the task
+ * until the next execution time happens.
+ * 
+ * NOTE: You can either return from the task or use this function, it's up to
+ * the user's liking.
+ */
+void sched_task_tick();
+
+static inline sched_task *sched_task_current() {
+    return scheduler.cur_task;
+}
+
+/**************************** Task privileged functions ***********************/
+// Can be called from syscall
+
+void sched_taskp_tick(sched_task *task);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
